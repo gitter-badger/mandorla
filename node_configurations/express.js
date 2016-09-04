6a5ea606846c1974fe9d263617dbd47890df321f
noidhoon <noidhoon@gmail.com>
2016-09-04 17:22:40 +0200

    node-sass-middleware, bourbon and neat integration

--- node_configurations/express.js
+++ node_configurations/express.js
@@ -1,11 +1,12 @@
 'use strict';
 
 module.exports = function(nconf) {
+
   var express = require('express');
   var application = express();
 
   var helmet = require('helmet');
-  application.use(helmet);
+  application.use(helmet());
 
   var session = require('express-session');
   var redis = require('connect-redis')(session);
@@ -17,9 +18,7 @@ module.exports = function(nconf) {
   }));
 
   var bodyparser = require('body-parser');
-  application.use(bodyparser.urlencoded({
-    extended: true
-  }));
+  application.use(bodyparser.urlencoded({ extended: true }));
   application.use(bodyparser.json());
 
   var compression = require('compression');
@@ -30,6 +29,20 @@ module.exports = function(nconf) {
 
   application.set('view engine', 'pug');
   application.set('views', './src/views');
+
+  var paths = require('bourbon').includePaths;
+  paths = paths.concat(require('bourbon-neat').includePaths)
+
+  var sass = require('node-sass-middleware');
+  application.use(sass({
+    src: './src/views',
+    dest: './node_statics',
+    includePaths: paths,
+    debug: true,
+    outputStyle: 'compressed',
+  }));
+
+  application.use(express.static('./node_statics'));
 
   return application;
 }
